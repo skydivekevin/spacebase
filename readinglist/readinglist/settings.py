68f06e1e9c8c166c5f68981eb79276c4e4ce7374
Kevin <kevin@bluematch.com>
2020-02-19 20:55:36 -0700

    configuring smtp for email password reset
    Signed-off-by: Kevin <kevin@bluematch.com>

--- readinglist/readinglist/settings.py
+++ readinglist/readinglist/settings.py
@@ -127,3 +127,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'spacebasecodechallenge@gmail.com'
+EMAIL_HOST_PASSWORD = 'denovrphtjufmmzh'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Kevin <noreply@spacebasecodechallenge.com>'
+
